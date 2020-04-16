daa6a03778417f466c7ed159b801dca8357b8059
honnnei <hannpop@gmail.com>
2020-04-16 11:29:42 +0200

    onChange added

--- movie-app-one/src/App.js
+++ movie-app-one/src/App.js
@@ -1,42 +1,80 @@
 import React from 'react';
 import HomePage from './containers/HomePage';
+// import {
+//   Link,
+//   Route,
+//   Switch,
+//   BrowserRouter as Router
+// } from "react-router-dom";
 
- class App extends React.Component {
-   constructor(props){
-     super(props);
-     this.state = {
-      selectedMovie : null
-   };
-   }
-
-   componentDidMount() {
-    this.getMovieData();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedMovie: null,
+      movieQuery: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  const allMoviesLink = `http://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`
-  const selectedMovieLink = `http://www.omdbapi.com/?t=${movieImbdId}&apikey=${apiKey}`
-  const movieImbdId = "";
-  const movieQuery = "";
-  const apiKey = "2480f3a";
-  getMovieData = () => {
-   fetch("http://www.omdbapi.com/?apikey=b91ba3c6&")
-     .then(response => response.json())
-     .then(result => this.setState(
-           {
-             selectedMovie: result
-           },
-         () => console.log("setState Completed", this.state.selectedMovie)
-       )
-     );
-  };
+  handleSubmit(event) {
+    
+   
 
+  }
+
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({movieQuery: event.target.value});
+    if (this.state.movieQuery.length > 5) {
+    const movieImbdId = "";
+    const apiKey = "2480f3a";
+    const allMoviesLink = `http://www.omdbapi.com/?s=${this.state.movieQuery}&apikey=${apiKey}`
+    // const selectedMovieLink = `http://www.omdbapi.com/?t=${movieImbdId}&apikey=${apiKey}`
+    // fetch(allMoviesLink)
+      .then(response => response.json())
+      .then(result => this.setState({
+          selectedMovie: result.Search
+        },
+        () => console.log("setState Completed", this.state.selectedMovie)
+      ));
+    }
+    
+
+  }
 
   render() {
-    return(
-      <HomePage />
+    return ( 
+      <div className = "App" >
+        <div className = "searchBox" >
+          <form className = "searchForm">
+            <label for = "searchTerm" > Search Movies: </label> 
+            <input type = "text" name = "searchTerm" value={this.state.movieQuery} onChange={this.handleChange}></input> 
+            <button type = "submit" > Search </button> 
+          </form> 
+        </div> 
+        <HomePage searchResults = {this.state.selectedMovie}/> 
+      </div>
     );
-     
+
   }
 }
 export default App;
 
+
+
+
+
+// <div className="navbar">
+// <Link to="/">Home</Link>
+// <Link to="/about">About</Link>
+// <Link to="/services">Services</Link>
+// <Link to="/movies">Movies</Link>
+
+// {/* <Route path="/" exact component={Index} /> */}
+// <Route path="/about" component={About} />
+// <Route path="/services" component={Services} />
+// <Route path="/movies" component={() => <Movies list={ list }/>} />
+// </div>
--- movie-app-one/src/containers/HomePage.js
+++ movie-app-one/src/containers/HomePage.js
@@ -2,22 +2,48 @@ import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Results from '../components/Results';
+import MovieResult from '../components/MovieResult';
 
 
 class HomePage extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-    // selectedMovie : null
+    searchedMovies : this.props.searchResults
     };
     }
 
+    
+
     render() {
+        let searchArray = [];
+        searchArray = this.props.searchResults;
+        console.log(searchArray);
+        let display;
+
+        onClick = (event) => { 
+            event.preventDefault();
+            
+            movieDisplay = <MovieInfo title={this.props.movie.Title} year={this.props.movie.Year} poster={this.props.movie.Poster} />
+            
+            console.log(this.props.movieIndex);
+        }
+        
+
+        if (!searchArray) {
+            display = "Loading";
+        } else {
+            display = searchArray.map((movie, index) => <MovieResult onClick={onClick} movie={movie} key={index} movieIndex={index} array={searchArray}/>)
+        }
+
         return(
             <div className="homepage">
-                <Header />
+                {/* <Header />
                 <Search />
-                <Results />
+                <Results /> */}
+                <div className="searchResults">
+                {display}
+                </div>
             </div>
             
         );
