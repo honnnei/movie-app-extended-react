6263f971a3f4a3f41a9e524849a56ffba521ba1b
honnnei <hannpop@gmail.com>
2020-04-16 22:07:58 +0200

    upvote buttons work

--- movie-app-two/src/components/MoviePoster.js
+++ movie-app-two/src/components/MoviePoster.js
@@ -1,18 +1,49 @@
 import React from 'react';
 
-function MoviePoster(props) {
-    let movieDetails;
-    if (!props.posterUrl) {
-        movieDetails = ".";
-    } else {
-        movieDetails = <div className="displayedMovieInfo"><h2>{props.movieTitle}</h2><h3>{props.movieYear}</h3><img src={props.posterUrl}></img></div>
+class MoviePoster extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            votes : 0
+        };
+      }
+    upVote = () => {
+        this.setState({
+            votes : this.state.votes + 1
+        });
     }
 
-    return(
-       <div>
-           {movieDetails}
-       </div>
-    );
+    downVote = () => {
+        this.setState({
+            votes : this.state.votes - 1
+        });
+    }
+
+
+    render() {
+        let movieDetails;
+        if (!this.props.posterUrl) {
+            movieDetails = ".";
+        } else {
+            movieDetails =
+            <div className="displayedMovieInfo">
+                <h2>{this.props.movieTitle}</h2>
+                <h3>{this.props.movieYear}</h3>
+                <img src={this.props.posterUrl}></img>
+                <button onClick={this.upVote}>Up Vote!</button>
+                <button onClick={this.downVote}>Down Vote!</button>
+                {this.state.votes}
+            </div>
+        }
+
+        return(
+        <div>
+            {movieDetails}
+        </div>
+        );
+
+    }
+   
 
 }
 
